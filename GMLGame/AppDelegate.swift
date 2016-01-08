c35fc6e04f1f719c4b0fd2049420172afd9acb6c
guominglong <guominglong@guominglongdemacbook-pro.local>
2016-01-08 14:00:12 +0800

    Initial Commit

---
+++ GMLGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GMLGame
+//
+//  Created by guominglong on 16/1/8.
+//  Copyright © 2016年 guominglong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GMLGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GMLGame
+//
+//  Created by guominglong on 16/1/8.
+//  Copyright © 2016年 guominglong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
